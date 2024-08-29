b36f1ddcd5103eb013919973cafd02132394d8b1
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-08-29 08:53:00 +0330

    - Hello World In Visual Basic Net

---
+++ HelloWorldVB.vb
@@ -0,0 +1,12 @@
+Imports System
+Module HelloWorldVB
+
+    Sub Main()
+
+        System.Console.WriteLine("Hello, Welcome to the world of VB.NET")
+        System.Console.WriteLine("Press any key to continue...")
+        System.Console.ReadKey()
+
+    End Sub
+
+End Module
